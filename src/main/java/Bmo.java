cc084162fbcd5d1b2007c770816063092b470305
moonmertens <e1334285@u.nus.edu>
2026-01-23 15:29:52 +0800

    Added list functionalities

--- src/main/java/Bmo.java
+++ src/main/java/Bmo.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Bmo {
 
     private static final String LINE = "________________________________________________________________";
     private static final String NAME = "BMO";
+
+    private static List<String> storage = new ArrayList<String>();
 
     public static void main(String[] args) {
         
@@ -13,18 +17,35 @@ public class Bmo {
         Scanner sc = new Scanner(System.in);
         
         while (true) {
+
+            // Get user input
             String input = sc.nextLine();
             System.out.println(LINE);
 
+            // Exit condition
             if (input.equals("bye")) {
                 Bmo.exit();
                 break;
             }
-            System.out.println(input);
-            System.out.println(LINE);
+            
+            Bmo.process(input);
+
         }
 
+        sc.close();
+    }
 
+    private static void process(String input) {
+        switch (input) {
+            case "list":
+                Bmo.list();
+                break;
+        
+            default:
+                Bmo.add(input);
+                break;
+        }
+        System.out.println(Bmo.LINE);
     }
 
     private static void greet() {
@@ -38,6 +59,15 @@ public class Bmo {
         System.out.println(LINE);
     }
 
+    private static void list() {
+        for (int i = 0; i < Bmo.storage.size(); i++) {
+            System.out.println((i + 1) + ". " + Bmo.storage.get(i));
+        }
+    }
 
+    private static void add(String input) {
+        Bmo.storage.add(input);
+        System.out.println("added: " + input);
+    }
 
 }
